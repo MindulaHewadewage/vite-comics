8def3cb06da827b0228f81b007f92d17c12dd94b
MindulaHewadewage <mindufernando@gmail.com>
2023-01-20 16:15:35 +0100

    struttura html header

---
+++ src/App.vue
@@ -0,0 +1,46 @@
+<script>
+
+export default {
+  data() {
+    return {
+
+    }
+  }
+}
+</script>
+
+<template>
+  <div class="container">
+    <header></header>
+    <main></main>
+    <footer></footer>
+  </div>
+
+</template>
+
+<style>
+/* reset */
+* {
+  box-sizing: border-box;
+  margin: 0;
+  padding: 0;
+}
+
+/* generics */
+img {
+  max-width: 100%;
+  width: 100%;
+  height: auto;
+}
+
+a {
+  color: black;
+  text-decoration: none;
+}
+
+/* utils */
+.container {
+  margin: 0 auto;
+  max-width: 1200px;
+}
+</style>
---
+++ src/components/AppHeader.vue
@@ -0,0 +1,22 @@
+<script>
+export default {
+    name: 'AppHeader',
+    data() {
+        return {
+
+        }
+    }
+};
+</script>
+
+
+<template>
+    <header>
+        <figure></figure>
+        <nav></nav>
+    </header>
+</template>
+
+<style>
+
+</style>
